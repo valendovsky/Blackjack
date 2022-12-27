7d33c6d4088ef592a1bccd90a173c10082e7a112
Tim1379 <semidolin.timur@mail.ru>
2022-12-27 17:32:33 +0400

    Just do it

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Карточная игра BlackJack
 
+list_settings = get_settings(file)
 
 def main():
+    list_settings = get_settings(file)
+    list_deck = get_deck(settings, deck)
+    shuffle = shuffle_deck(settings, deck, point_shuffle)
+    print_deck(deck)
+    play_blackjack(deck, settings.console, players)
+    print_deck(deck)
     pass
 
 
