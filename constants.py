4a27fad510808fdcd89c927bc28fe2d89cfe1501
Valendovsky <valendovsky@gmail.com>
2023-01-06 22:11:58 +0400

    Create get_settings(), constants.py and .gitignore

---
+++ constants.py
@@ -0,0 +1,19 @@
+# File adress
+FILE_ADDRESS = 'source/file_settings.txt'
+
+# BlackJack settings
+DECK_NUM = 'deck_num'
+POINT_SHUFFLE = 'point_shuffle'
+CONNECTION = 'connection'
+PLAYERS_NUM = 'players_num'
+MIN_BET = 'min_bet'
+MAX_BET = 'max_bet'
+
+# Settings value
+CONSOLE = 'console'
+NETWORK = 'network'
+DECK_VALUE = 6
+SHUFFLE_MIN = 3.5
+SHUFFLE_MAX = 4.5
+PLAYERS_MIN = 1
+PLAYERS_MAX = 7
--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 # Карточная игра BlackJack
+from constants import *
+
+
+# This function reads information from a settings text file and checks the received data for errors.
+def get_settings():
+    with open(FILE_ADDRESS, 'r') as rd:
+        lst = rd.readlines()
+
+    dict_settings = {DECK_NUM: DECK_VALUE}
+    # The next block checks the received information by data type
+    try:
+        dict_settings[POINT_SHUFFLE] = float(lst[0])
+        dict_settings[CONNECTION] = str.rstrip(str(lst[1]))
+        dict_settings[PLAYERS_NUM] = int(lst[2])
+        dict_settings[MIN_BET] = int(lst[3])
+        dict_settings[MAX_BET] = int(lst[4])
+    except ValueError:
+        print('Некорректное значение в файле настроек! 1,4,5,6 - это целые числа, 2 - число с точкой, а 3 - строчное выражение.')
+        exit()
+
+    # The next block checks the received information for logical errors.
+    if dict_settings[POINT_SHUFFLE] < SHUFFLE_MIN or dict_settings[POINT_SHUFFLE] > SHUFFLE_MAX:
+        print('Диапазон точки шаффла должен быть между 3,5 и 4,5 колоды, надо исправить!')
+    elif dict_settings[CONNECTION] != CONSOLE and dict_settings[CONNECTION] != NETWORK:
+        print('Соединение должно быть либо console либо network, надо исправить!')
+    elif dict_settings[PLAYERS_NUM] < PLAYERS_MIN or dict_settings[PLAYERS_NUM] > PLAYERS_MAX:
+        print('Количество игроков не может быть меньше 1 и больше 7. Надо исправить!')
+    elif dict_settings[MIN_BET] > dict_settings[MAX_BET] or dict_settings[MIN_BET] <= 0:
+        print('Значение минимальной ставки не может быть больше значения максимальной или ноль и меньше. Надо исправить!')
+    else:
+        return dict_settings
+    exit()
 
 
 def main():
-    pass
+    settings = get_settings()
+
+    # Debug
+    print('Debug #1: ', settings)
 
 
 if __name__ == '__main__':
